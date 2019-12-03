7e65ad5680a10dc08d5863ce3ca01d9ea1a19b87
Rafał Głuszak <rafal.gluszak@gmail.com>
2019-12-03 01:14:24 +0100

    Trello 6: Install dependencies.

---
+++ src/Controller/InfoController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class InfoController
+ * @package App\Controller
+ *
+ * @author Rafał Głuszak <rafal.gluszak@gmail.com>
+ */
+class InfoController extends AbstractController
+{
+
+}
